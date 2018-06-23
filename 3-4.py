d77c30e394f10d9c64d39f0a78ed43969b07f9eb
gnanesh <gnaneshkunal@outlook.com>
2018-06-23 01:56:52 +0000

    Retrieving a transaction and iterating its outputs

---
+++ 3-4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from bitcoin.rpc import RawProxy
+
+p = RawProxy()
+
+# Alice's transaction ID
+txid = "0627052b6f28912f2703066a912ea577f2ce4da4caa5a5fbd8a57286c345c2f2"
+
+# First, retrieve the raw transcation in hex
+raw_tx = p.getrawtransaction(txid)
+
+# Decode the transaction hex into a JSON object
+
+decoded_tx = p.decoderawtransaction(raw_tx)
+
+# Retrieve teach of the outputs from the transaction
+for output in decoded_tx['vout']:
+    print(output['scriptPubKey']['addresses'], output['value'])
+    
